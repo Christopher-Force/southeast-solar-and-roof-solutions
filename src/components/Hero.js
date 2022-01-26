b5cc78da84f8f8efa9e72d8634f319f12af94c4c
Christopher-Force <chrisforceproduction@outlook.com>
2022-01-26 11:58:13 -0500

    added animated logo placeholder

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
         <div className="relative bg-black z-10">
             <div classname="">
             <div className="md:mx-20 grid justify-items-center">
-                <video className="max-w-6xl" style={{objectFit: "fill", width: "100%"}} autoPlay muted playsInline>
+                <video className="max-w-6xl" style={{objectFit: "fill", width: "100%"}} autoPlay muted playsInline poster="../assets/img/Logo dark.png">
                     <source id="myvideo" src={logoAnimated} type="video/mp4" />
                 </video>
             </div>
