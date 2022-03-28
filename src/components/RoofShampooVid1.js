8011d8c9057c20a525259adc7030b4c730328268
Christopher Force <chrisforceproduction@outlook.com>
2022-03-27 21:12:44 -0400

    fixed autoplay issues

--- src/components/RoofShampooVid1.js
+++ src/components/RoofShampooVid1.js
@@ -4,7 +4,7 @@ export default function RoofShampooVid1 () {
     return(
         <div className="relative bg-black z-9 ">
                 <div className="md:mx-20 grid justify-items-center">
-                    <video className="max-w-6xl max-h-6xl" style={{objectFit: "fill", width: "100%"}} autoPlay controls playsInline poster="../assets/img/Logo dark.png">
+                    <video className="max-w-6xl max-h-6xl" style={{objectFit: "fill", width: "100%"}} autoPlay controls muted playsInline poster="../assets/img/Logo dark.png">
                         <source id="myvideo" src={RoofShampoo1} type="video/mp4" />
                     </video>
                 </div>
