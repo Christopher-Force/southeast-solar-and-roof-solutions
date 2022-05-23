ea8e56a61d6536b005a037ef17ac49efb2a71c12
Christopher Force <chrisforceproduction@outlook.com>
2022-05-23 11:18:12 -0400

    final cleanup of roof shampoo landing page and thank you page. Ready to rock n roll.

--- src/modules/ThankYou.js
+++ src/modules/ThankYou.js
@@ -1,19 +1,17 @@
 import thankYou from "../assets/img/thankyou.png"
+import "./thankyou.css"
 
 export default function ThankYou () {
     return (
-        <div  className="bg-slate-900">
-            <h2 className="text-orange-400 lg:text-3xl title-font font-bold text-center mt-6 overflow-x-hidden mx-auto">
-                Thank You! An associate will contact you within 24 hours.
+        <div  className="bg-slate-900 py-6 lg:py-12">
+            <h2 className="text-orange-400 lg:text-4xl title-font font-bold text-center overflow-x-hidden mx-auto">
+                Thank You For Your Time!
             </h2>
-            <div className="flex flex-col items-center text-center justify-center ">
-                <div className="w-2/5 lg:w-2/5 h-1 bg-orange-400 rounded mb-4" />
-            </div>
-            <h2 className="text-orange-400 lg:text-3xl title-font font-bold text-center mt-2 mb-4 overflow-x-hidden mx-auto">
-                Click on the picture below to be directed to the main website.
+            <h2 className="text-orange-400 lg:text-3xl title-font font-medium text-center mt-2 mb-4 overflow-x-hidden mx-auto">
+            An associate will contact you within 24 hours.<br />Click on the picture below to be directed to the main website.
             </h2>
             
-            <div className="flex justify-center"><a href="https://www.southeastsolarsolutions.com"><img src={thankYou} alt="thank you" className="max-h-[40rem] xl:max-h-[48rem] flex justify-center" /></a></div>
+            <div className="flex justify-center no-bluring" ><a href="https://www.southeastsolarsolutions.com"><img src={thankYou} alt="thank you" className="max-h-[38rem] xl:max-h-[48rem] flex justify-center border-2" /></a></div>
         </div>
     )
 }
